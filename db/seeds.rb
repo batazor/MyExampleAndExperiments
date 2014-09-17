cdffa22cc1243f5d780e594887dc3c402d1e123c
Login Viktor <batazor111@gmail.com>
2014-09-18 00:48:58 +0400

    Generate Scaffold for User and Post, Change root for Post's Index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+
+User.create(
+  name: "Ivanov",
+  email: "ivan@gmail.com",
+  location: "ru",
+  description: "Лентяй, лежебока"
+)
+
+User.create(
+  name: "Petr",
+  email: "petr.uk@mail.ru",
+  location: "ua",
+  description: "Кобачки, лампочка, европа..."
+)
+
+Post.delete_all
+
+Post.create(
+  content: "Новость, новость, новость, новость",
+  user_id: 1
+)
+
+Post.create(
+  content: "Сенсация, сенсация, сенсация",
+  user_id: 1
+)
