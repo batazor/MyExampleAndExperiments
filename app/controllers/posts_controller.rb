6fbb07f6c39428466f0252ec87a7c7477fba386e
Login Viktor <batazor111@gmail.com>
2014-09-18 03:09:48 +0400

    Personalize flash message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :get_users, :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
@@ -67,6 +67,10 @@ class PostsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_post
       @post = Post.find(params[:id])
+    end
+
+    def get_users
+      @users = User.all.map {|user| [user.name, user.id]}
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flass_class type
+    case type
+      when :success then "success"
+      when :error   then "warning"
+      when :alert   then "warning"
+      when :notice  then ""
+      else "info"
+    end
+  end
 end
