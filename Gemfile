386e93c1c9516d086d81aa7b9248416dadd95dec
Login Viktor <batazor111@gmail.com>
2014-09-19 23:40:18 +0400

    Install Devise Gem, Add Navigation Bar Links

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'turbolinks',   '~> 2.3.0'
 gem 'jbuilder',     '~> 2.0'
 gem 'foundation-rails'
 gem 'sdoc',         '~> 0.4.0', group: :doc
+gem 'devise'
 
 group :development do
   gem 'sqlite3',    '~> 1.3.9'
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -18,7 +18,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { description: @user.description, email: @user.email, location: @user.location, name: @user.name }
+      post :create, user: { description: @user.description, location: @user.location, name: @user.name }
     end
 
     assert_redirected_to user_path(assigns(:user))
@@ -35,7 +35,7 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should update user" do
-    patch :update, id: @user, user: { description: @user.description, email: @user.email, location: @user.location, name: @user.name }
+    patch :update, id: @user, user: { description: @user.description, location: @user.location, name: @user.name }
     assert_redirected_to user_path(assigns(:user))
   end
 
