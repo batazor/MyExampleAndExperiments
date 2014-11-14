67a20f45d8cb986bfb2ab9b0745fb839f2677118
Login Viktor <batazor111@gmail.com>
2014-11-15 00:25:09 +0300

    Create layout admin. New controller admin and view main

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+
   devise_for :users
 
   devise_scope :user do
     get "admin", to: "devise/sessions#new"
+  end
+
+  namespace :admin do
+  end
+
+  scope '/admin' do
+    get '/main', to: 'admin#main'
   end
 
   root 'welcome#main'
