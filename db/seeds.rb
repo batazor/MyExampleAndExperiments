a1117cf2ba570306be008bd5f099f6d1dd807d40
Login Viktor <batazor111@gmail.com>
2014-11-16 23:54:59 +0300

    Update seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+Category.delete_all
 Category.create(title: 'Default',
-                description: 'default category',
+                description: '<p>default category</p>',
                 id: 1)
 
+Post.delete_all
 Post.create(title: 'Hello World',
-            text: 'Hello! I\'m simple blog service.',
+            text: '<p>Hello! I\'m simple blog service.</p>',
             category_id: 1)
+
+User.delete_all
+User.create(id: 1,
+            email: 'admin@admin.com',
+            encrypted_password: '$2a$10$PY3IrDRZIKobZbzODFxy6OcTMm/ZIA9yKUSIinREeleYr1PMGDqJ2')
