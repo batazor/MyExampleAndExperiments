1279b6fe130672df4a77ee91c7fb28b2c9fbbc3b
Login Viktor <batazor111@gmail.com>
2014-11-28 02:40:10 +0300

    Added port selection and ip-address

--- client.cpp
+++ client.cpp
@@ -1,17 +1,53 @@
 #include <cstdio>
 #include <cstdlib>
 #include <unistd.h>
+#include <cstring>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
 char message[] = "Hello world!\n";
 char buf[sizeof(message)];
 
-int main() {
+int main(int argc, char *argv[]) {
+  int port = 8080;
+  char ip_addr[16] = "127.0.0.1";
   int sock;
-  struct sockaddr_in addr;
-  
+  struct sockaddr_in addr = {};
+
+  if (argc == 3) {
+    if (strcmp(argv[1], "-p") == 0)  { port = atoi(argv[2]); }
+
+    if (strcmp(argv[1], "-ip") == 0) {
+      if (inet_addr(argv[2]) != -1) {
+        addr.sin_addr.s_addr = inet_addr(argv[2]);
+        strcpy(ip_addr, argv[2]);
+      }
+    } else {
+      addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    }
+  } else if (argc == 5) {
+    if (strcmp(argv[1], "-p") == 0)  { port = atoi(argv[2]); }
+    if (strcmp(argv[3], "-p") == 0)  { port = atoi(argv[4]); }
+
+    if (strcmp(argv[1], "-ip") == 0) {
+      if (inet_addr(argv[2]) != -1) {
+        addr.sin_addr.s_addr = inet_addr(argv[2]);
+        strcpy(ip_addr, argv[2]);
+      }
+    } else if (strcmp(argv[3], "-ip") == 0) {
+      if (inet_addr(argv[4]) != -1) {
+        addr.sin_addr.s_addr = inet_addr(argv[4]);
+        strcpy(ip_addr, argv[4]);
+      }
+    } else {
+      addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    }
+  } else {
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  }
+
   sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock < 0) {
     perror("socket");
@@ -19,17 +55,18 @@ int main() {
   }
 
   addr.sin_family = AF_INET;
-  addr.sin_port = htons(8080);
-  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+  addr.sin_port = port;
   if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
     perror("connect");
     exit(2);
   }
 
+  printf("Server run in %s:%d\n", ip_addr, addr.sin_port);
+
   send(sock, message, sizeof(message), 0);
   recv(sock, buf, sizeof(message), 0);
 
-  std::printf("%s", buf);
+  printf("%s", buf);
   close(sock);
 
   return 0;
--- server.cpp
+++ server.cpp
@@ -1,13 +1,17 @@
 #include <cstdio>
 #include <cstdlib>
 #include <unistd.h>
+#include <cstring>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 
-int main() {
+int main(int argc, char *argv[]) {
+  int port = 8080;
+  char ip_addr[16] = "127.0.0.1";
   int sock, listener;
-  struct sockaddr_in addr;
+  struct sockaddr_in addr = {};
   char buf[1024];
   int bytes_read;
 
@@ -17,14 +21,40 @@ int main() {
     exit(1);
   }
 
+  if (argc == 3) {
+    if (strcmp(argv[1], "-p") == 0)  { port = atoi(argv[2]); }
+
+    if (strcmp(argv[1], "-ip") == 0) {
+      if (inet_addr(argv[2]) != -1) {
+        addr.sin_addr.s_addr = inet_addr(argv[2]);
+        strcpy(ip_addr, argv[2]);
+      }
+    }
+  } else if (argc == 5) {
+    if (strcmp(argv[1], "-p") == 0)  { port = atoi(argv[2]); }
+    if (strcmp(argv[3], "-p") == 0)  { port = atoi(argv[4]); }
+
+    if (strcmp(argv[1], "-ip") == 0) {
+      if (inet_addr(argv[2]) != -1) {
+        addr.sin_addr.s_addr = inet_addr(argv[2]);
+        strcpy(ip_addr, argv[2]);
+      }
+    } else if (strcmp(argv[3], "-ip") == 0) {
+      if (inet_addr(argv[4]) != -1) {
+        addr.sin_addr.s_addr = inet_addr(argv[4]);
+        strcpy(ip_addr, argv[4]);
+      }
+    }
+  }
+
   addr.sin_family = AF_INET;
-  addr.sin_port = htons(8080);
-  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_port = port;
   if (bind(listener, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
     perror("socket");
     exit(3);
   }
 
+  printf("Server run in %s:%d\n", ip_addr, addr.sin_port);
   listen(listener, 1);
 
   while(1) {
