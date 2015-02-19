260029ee5098d4fb8826f8652fb5360175f7b78b
Login Viktor <batazor111@gmail.com>
2015-02-19 14:52:34 +0300

    Gulp. Fix boot order css in task 'css'

--- gulpfile.js
+++ gulpfile.js
@@ -67,7 +67,12 @@ gulp.task('sass', function() {
 
 // Build CSS
 gulp.task('css', ['sass', 'stylus'], function() {
-  return gulp.src(watch.css)
+  return gulp.src([
+      path.css + "/materialize.min.css",
+      path.css + "/build-sass.css",
+      path.css + "/build-stylus.css",
+      watch.css
+    ])
     .on('error', console.log)
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(myth())
