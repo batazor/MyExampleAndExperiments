ea6890c9832e382feb54d5e9517dc93ee65ac8cf
Login Viktor <batazor111@gmail.com>
2015-05-26 02:36:04 -0700

    Step 2. Find/Insert Message to MongoDB

--- client/app.js
+++ client/app.js
@@ -1,11 +1,1 @@
-Template.messages.helpers({
-  messages: [
-    { text: "All these messages" },
-    { text: "Uses the same template" },
-    { text: "But have a different data context" },
-    { text: "It's why these messages are all different!" },
-    { text: "Hey!" },
-    { text: "What's up man!" },
-    { text: "Hello!" }
-  ]
-});
+Template.messages.helpers({ messages: Messages.find({}) });
