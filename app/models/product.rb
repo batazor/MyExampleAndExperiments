2322d93ebce782d8ce6c6f56ee9186470f00f38b
Login Viktor <batazor111@gmail.com>
2015-05-30 07:20:44 -0700

    Vilidation Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ActiveRecord::Base
+
+  validates :title, :description, :image_url, presence: { message: "can't be empty" }
+  validates :title, uniqueness: true, length: { minimum: 10 }
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'The URL must point to the image format GIF, JPG or PNG.'
+  }
+
 end
