07398a8958b2d138eaef4555401191105a45f7d9
Login Viktor <batazor111@gmail.com>
2015-07-19 15:31:18 +0300

    Unit Testing

--- main.go
+++ main.go
@@ -1,32 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-
-	"gopkg.in/unrolled/render.v1"
 )
 
+// HelloWorld -> for test
+func HelloWorld(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(res, "Hello World")
+}
+
 func main() {
-	r := render.New(render.Options{})
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("Welcome, visit sub pages now."))
-	})
-
-	mux.HandleFunc("/data", func(w http.ResponseWriter, req *http.Request) {
-		r.Data(w, http.StatusOK, []byte("Some binary data here."))
-	})
-
-	mux.HandleFunc("/json", func(w http.ResponseWriter, req *http.Request) {
-		r.JSON(w, http.StatusOK, map[string]string{"hello": "json"})
-	})
-
-	mux.HandleFunc("/html", func(w http.ResponseWriter, req *http.Request) {
-		// Assumes you have a template in ./templates called "example.tmpl"
-		// $ mkdir -p templates && echo "<h1>Hello {{.}}.</h1>" > templates/example.tmpl
-		r.HTML(w, http.StatusOK, "example", nil)
-	})
-
-	http.ListenAndServe(":8080", mux)
+	http.HandleFunc("/", HelloWorld)
+	http.ListenAndServe(":3000", nil)
 }
