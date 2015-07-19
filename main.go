c34fb70fd69d2749943f70a893909623b08ad5b0
Login Viktor <batazor111@gmail.com>
2015-07-19 17:08:06 +0300

    Databases

--- main.go
+++ main.go
@@ -1,41 +1,44 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
 
-	"gopkg.in/unrolled/render.v1"
+	_ "github.com/mattn/go-sqlite3"
 )
 
-// Action defines a standard function signature for us to use when creating
-// controller actions. A controller action is basically just a method attached to
-// a controller.
-type Action func(rw http.ResponseWriter, r *http.Request) error
+func main() {
+	db := NewDB()
+	log.Println("Listening on :8080")
+	http.ListenAndServe(":8080", ShowBooks(db))
+}
 
-// AppController -> This is our Base controller
-type AppController struct{}
-
-// Action -> The action function helps with error handling in a controller
-func (c *AppController) Action(a Action) http.Handler {
+// ShowBooks ...
+func ShowBooks(db *sql.DB) http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-		if err := a(rw, r); err != nil {
-			http.Error(rw, err.Error(), 500)
+		var title, author string
+		err := db.QueryRow("select title, author from books").Scan(&title, &author)
+		if err != nil {
+			panic(err)
 		}
+
+		fmt.Fprintf(rw, "the first book is '%s' by '%s'", title, author)
 	})
 }
 
-// MyController ...
-type MyController struct {
-	AppController
-	*render.Render
-}
+// NewDB ...
+func NewDB() *sql.DB {
+	db, err := sql.Open("sqlite3", "example.sqlite")
+	if err != nil {
+		panic(err)
+	}
 
-// Index ...
-func (c *MyController) Index(rw http.ResponseWriter, r *http.Request) error {
-	c.JSON(rw, 200, map[string]string{"Hello": "JSON"})
-	return nil
-}
+	_, err = db.Exec("create table if not exists books(title text, author text)")
+	if err != nil {
+		panic(err)
+	}
 
-func main() {
-	c := &MyController{Render: render.New(render.Options{})}
-	http.ListenAndServe(":8080", c.Action(c.Index))
+	return db
 }
