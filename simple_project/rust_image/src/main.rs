0fe0610289c6ed71314535ff1389ecdf61b20abe
Login Viktor <batazor111@gmail.com>
2015-08-12 08:41:34 +0300

    RUST_IMAGE: Finaly

--- simple_project/rust_image/src/main.rs
+++ simple_project/rust_image/src/main.rs
@@ -13,40 +13,28 @@ mod model;
 
 use model::Model;
 use geometry::Vector3D;
-use tgacanvas::TgaCanvas;
+use sdlcanvas::SdlCanvas;
 use canvas::Canvas;
 
-//const WHITE: u32 = 0xFFFFFF;
-//const RED: u32 = 0xFF0000;
-//const BLUE: u32 = 0x0000FF;
-//const GREEN: u32 = 0x00FF00;
 const WIDTH: usize = 700;
 const HEIGHT: usize = 700;
 const DEPTH: usize = 255;
-
-fn get_gray(intensity: f32) -> u32 {
-    debug!("intensity is {}", intensity);
-    let mut result = (255.0*intensity) as u32;
-    result += (255.0*intensity) as u32*256;
-    result += (255.0*intensity) as u32*256*256;
-    debug!("result is {:X}", result);
-    return result;
-}
 
 fn main() {
     env_logger::init().unwrap();
     info!("starting up");
     let light_direction = Vector3D::new(0.0, 0.0, -1.0);
-    let model = Model::new("obj_african/african_head.obj");
-    let mut canvas: TgaCanvas = Canvas::new(WIDTH, HEIGHT);
+    let mut model = Model::new("obj_african/african_head.obj");
+    let mut canvas: SdlCanvas = Canvas::new(WIDTH, HEIGHT);
     info!("drawing model");
-    for face in model.faces {
+    for i in 0..model.faces.len() {
+        let face = model.faces[i];
         debug!("processing face:");
-        debug!("({}, {}, {})", face[0], face[1], face[2]);
+        debug!("({}, {}, {})", face[0][0], face[1][0], face[2][0]);
         let mut p: [Vector3D<i32>; 3] = [Vector3D::new(0, 0, 0); 3];
         let mut world_p: [Vector3D<f32>; 3] = [Vector3D::new(0.0, 0.0, 0.0); 3];
         for j in 0..3 {
-            world_p[j] = model.vertices[face[j] as usize];
+            world_p[j] = model.vertices[face[j][0] as usize];
             p[j].x = ((world_p[j].x+1.)*WIDTH as f32/2.) as i32;
             p[j].y = ((world_p[j].y+1.)*HEIGHT as f32/2.) as i32;
             p[j].z = ((world_p[j].z+1.)*DEPTH as f32/2.) as i32;
@@ -55,7 +43,7 @@ fn main() {
         let n = n.normalized(1.0);
         let intensity = n*light_direction;
         if intensity>0.0 {
-            canvas.triangle(p[0], p[1], p[2], get_gray(intensity));
+            canvas.triangle(p[0], p[1], p[2], model.uv(i, 0), model.uv(i, 1), model.uv(i, 2), intensity, &mut model.diffusemap);
         }
     }
     info!("drawing result");
--- simple_project/rust_image/src/model.rs
+++ simple_project/rust_image/src/model.rs
@@ -2,10 +2,14 @@ use geometry::Vector3D;
 use std::io::{BufReader,BufRead};
 use std::fs::File;
 use std::path::Path;
+use tgacanvas::TgaCanvas;
+use canvas::Canvas;
 
 pub struct Model {
     pub vertices: Vec<Vector3D<f32>>,
-    pub faces : Vec<[i32; 3]>,
+    pub faces : Vec<[[i32; 3]; 3]>,
+    pub uv : Vec<[f32; 2]>,
+    pub diffusemap : TgaCanvas,
 }
 
 impl Model {
@@ -14,6 +18,7 @@ impl Model {
         let file = BufReader::new(File::open(&path).unwrap());
         let mut vertices = Vec::new();
         let mut faces = Vec::new();
+        let mut uv = Vec::new();
         for line in file.lines() {
             let line = line.unwrap();
             if line.starts_with("v ") {
@@ -23,19 +28,36 @@ impl Model {
                                             words[3].parse().unwrap()));
                 debug!("readed vertex: {}", vertices.last().unwrap());
             } else if line.starts_with("f ") {
-                let mut face: [i32; 3] = [-1, -1, -1];
+                let mut face: [[i32; 3]; 3] = [[-1; 3]; 3];
                 let words: Vec<&str> = line.split_whitespace().collect();
                 for i in 0..3 {
-                    face[i] = words[i+1].split("/").next().unwrap().parse().unwrap();
-                    face[i] -= 1;
-                    debug!("face[{}] = {}", i, face[i]);
+                    let mut j = 0;
+                    for num in words[i+1].split("/") {
+                        face[i][j] = num.parse::<i32>().unwrap() - 1;
+                        j += 1;
+                    }
+                    debug!("face[{}] = [{}, {}, {}]", i, face[i][0], face[i][1], face[i][2]);
                 }
                 faces.push(face);
+            } else if line.starts_with("vt ") {
+                let words: Vec<&str> = line.split_whitespace().collect();
+                uv.push([words[1].parse().unwrap(), words[2].parse().unwrap()]);
+                debug!("uv: [{}, {}]", uv.last().unwrap()[0], uv.last().unwrap()[1]);
             }
         }
+        let texture_path = file_path.rsplitn(2, '.').last().unwrap().to_string() + "_diffuse.tga";
+        info!("loading texture from path: {}", texture_path);
         Model {
             vertices: vertices,
             faces: faces,
+            uv: uv,
+            diffusemap: TgaCanvas::read(texture_path.split("*").next().unwrap()),
         }
     }
+
+    pub fn uv(&self, iface: usize, nvert: usize) -> Vector3D<i32> {
+        let idx = self.faces[iface][nvert][1] as usize;
+        return Vector3D::new(self.uv[idx][0] * self.diffusemap.xsize() as f32, self.uv[idx][1] * self.diffusemap.ysize() as f32, 0.0).to::<i32>();
+    }
+
 }
