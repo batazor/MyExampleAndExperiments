49450486dec1e4d8c27df870b0e80fcdd0cc326f
Login Viktor <batazor111@gmail.com>
2015-08-17 20:21:24 +0300

    Add graphql-nodejs-newsfeed

---
+++ example/Schema.js
@@ -0,0 +1,93 @@
+import {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLList
+} from 'graphql';
+
+const Story = new GraphQLObjectType({
+  name: 'Story',
+  fields: () => ({
+    id: {
+      type: GraphQLID
+    },
+    text: {
+      type: GraphQLString
+    },
+    author: {
+      type: User,
+      resolve(parent, args, {db}) {
+        return db.get(`
+          SELECT * FROM User WHERE id = $id
+        `, {$id: parent.author});
+      }
+    }
+  })
+});
+
+const User = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: {
+      type: GraphQLID
+    },
+    name: {
+      type: GraphQLString
+    },
+    stories: {
+      type: new GraphQLList(Story),
+      resolve(parent, args, {db}) {
+        return db.all(`
+          SELECT * FROM Story WHERE author = $user
+        `, {$user: parent.id});
+      }
+    }
+  })
+});
+
+const Query = new GraphQLObjectType({
+  name: 'Query',
+  fields: () => ({
+    viewer: {
+      type: User,
+      resolve(parent, args, {db, userId}) {
+        return db.get(`
+          SELECT * FROM User WHERE id = $id
+        `, {id: userId});
+      }
+    },
+    user: {
+      type: User,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID)
+        }
+      },
+      resolve(parent, {id}, {db}) {
+        return db.get(`
+          SELECT * FROM User WHERE id = $id
+          `, {$id: id});
+      }
+    },
+    story: {
+      type: Story,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID)
+        }
+      },
+      resolve(parent, {id}, {db}) {
+        return db.get(`
+          SELECT * FROM Story WHERE id = $id
+          `, {$id: id});
+      }
+    }
+  })
+});
+
+const Schema = new GraphQLSchema({
+  query: Query
+});
+export default Schema;
