04bcb02615bf7494e07b6f9b88c103fb203993e6
Login Viktor <batazor111@gmail.com>
2015-08-22 22:19:38 +0300

    Lesson 27. Create module p0271getintentaction (GetIntentAction)

---
+++ p0271getintentaction/src/main/java/ru/startandroid/getintentaction/Info.java
@@ -0,0 +1,70 @@
+package ru.startandroid.getintentaction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
+public class Info extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // get Intent, which caused it Activity
+        Intent intent = getIntent();
+        // it read from action
+        String action = intent.getAction();
+
+        String format = "", textInfo = "";
+
+        // depending on the action fill the variables
+        if (action.equals("ru.startandroid.intent.action.showtime")) {
+            format = "HH:mm:ss";
+            textInfo = "Time: ";
+        }
+        else if (action.equals("ru.startandroid.intent.action.showdate")) {
+            format = "dd.MM.yyyy";
+            textInfo = "Date: ";
+        }
+
+        // Depending on the contents of the variable format
+        // Get a date or time variable datetime
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        String datetime = sdf.format(new Date(System.currentTimeMillis()));
+
+        TextView tvDate = (TextView) findViewById(R.id.tvInfo);
+        tvDate.setText(textInfo + datetime);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
