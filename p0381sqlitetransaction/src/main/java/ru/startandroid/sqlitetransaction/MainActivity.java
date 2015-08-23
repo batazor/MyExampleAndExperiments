f6f919fff99e258a99d87761c502a3c8a875dc02
Login Viktor <batazor111@gmail.com>
2015-08-23 22:09:34 +0300

    Lesson 38. Create module p0381sqlitetransaction (SQLiteTransaction)

---
+++ p0381sqlitetransaction/src/main/java/ru/startandroid/sqlitetransaction/MainActivity.java
@@ -0,0 +1,106 @@
+package ru.startandroid.sqlitetransaction;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+
+    DBHelper dbh;
+    SQLiteDatabase db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(LOG_TAG, "--- onCreate Activity ---");
+        dbh = new DBHelper(this);
+        myActions();
+    }
+
+    void myActions() {
+        db = dbh.getWritableDatabase();
+        delete(db, "mytable");
+        insert(db, "mytable", "val1");
+        read(db, "mytable");
+        dbh.close();
+    }
+
+    void insert(SQLiteDatabase db, String table, String value) {
+        Log.d(LOG_TAG, "Insert in table " + table + " value = " + value);
+        ContentValues cv = new ContentValues();
+        cv.put("val", value);
+        db.insert(table, null, cv);
+    }
+
+    void read(SQLiteDatabase db, String table) {
+        Log.d(LOG_TAG, "Read table " + table);
+        Cursor c = db.query(table, null, null, null, null, null, null);
+        if (c != null) {
+            Log.d(LOG_TAG, "Records count = " + c.getCount());
+            if (c.moveToFirst()) {
+                do {
+                    Log.d(LOG_TAG, c.getString(c.getColumnIndex("val")));
+                } while (c.moveToNext());
+            }
+            c.close();
+        }
+    }
+
+    void delete(SQLiteDatabase db, String table) {
+        Log.d(LOG_TAG, "Delete all from table " + table);
+        db.delete(table, null, null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context) {
+            super(context, "myDB", null, 1);
+        }
+
+        public void onCreate(SQLiteDatabase db) {
+            Log.d(LOG_TAG, "--- onCreate database ---");
+
+            db.execSQL("create table mytable ("
+                    + "id integer primary key autoincrement,"
+                    + "val text"
+                    + ");");
+        }
+
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+}
