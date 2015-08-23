99c81f2e1bb90abf71152387858d305080a8dc4e
Login Viktor <batazor111@gmail.com>
2015-08-23 22:12:24 +0300

    Lesson 38. Update module p0381sqlitetransaction (SQLiteTransaction + transaction + lock)

--- p0381sqlitetransaction/src/main/java/ru/startandroid/sqlitetransaction/MainActivity.java
+++ p0381sqlitetransaction/src/main/java/ru/startandroid/sqlitetransaction/MainActivity.java
@@ -29,14 +29,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void myActions() {
-        db = dbh.getWritableDatabase();
-        delete(db, "mytable");
-        db.beginTransaction();
-        insert(db, "mytable", "val1");
-        db.endTransaction();
-        insert(db, "mytable", "val2");
-        read(db, "mytable");
-        dbh.close();
+        try {
+            db = dbh.getWritableDatabase();
+            delete(db, "mytable");
+
+            db.beginTransaction();
+            insert(db, "mytable", "val1");
+
+            Log.d(LOG_TAG, "create DBHelper");
+            DBHelper dbh2 = new DBHelper(this);
+            Log.d(LOG_TAG, "get db");
+            SQLiteDatabase db2 = dbh2.getWritableDatabase();
+            read(db2, "mytable");
+            dbh2.close();
+
+            db.setTransactionSuccessful();
+            db.endTransaction();
+
+            read(db, "mytable");
+            dbh.close();
+
+        } catch (Exception ex) {
+            Log.d(LOG_TAG, ex.getClass() + " error: " + ex.getMessage());
+        }
     }
 
     void insert(SQLiteDatabase db, String table, String value) {
