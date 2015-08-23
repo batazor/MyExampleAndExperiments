2be6942038dbc148ba139cc6ef27289330293894
Login Viktor <batazor111@gmail.com>
2015-08-23 10:40:55 +0300

    Lesson 28. Create module p0281intentextras (IntentExtras)

---
+++ p0281intentextras/src/main/java/ru/startandroid/intentextras/MainActivity.java
@@ -0,0 +1,60 @@
+package ru.startandroid.intentextras;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etFName;
+    EditText etLName;
+
+    Button btnSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etFName = (EditText) findViewById(R.id.etFName);
+        etLName = (EditText) findViewById(R.id.etLName);
+
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, ViewActivity.class);
+        intent.putExtra("fname", etFName.getText().toString());
+        intent.putExtra("lname", etLName.getText().toString());
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
