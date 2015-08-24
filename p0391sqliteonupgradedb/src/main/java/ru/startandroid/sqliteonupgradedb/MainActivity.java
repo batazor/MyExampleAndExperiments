c1eeadf8752dd6afcdcd427a1fb2795ce305fa41
Login Viktor <batazor111@gmail.com>
2015-08-24 07:59:19 +0300

    Lesson 39. Update module p0391sqliteonupgradedb (SQLiteOnUpgradeDB + DB version 2)

--- p0391sqliteonupgradedb/src/main/java/ru/startandroid/sqliteonupgradedb/MainActivity.java
+++ p0391sqliteonupgradedb/src/main/java/ru/startandroid/sqliteonupgradedb/MainActivity.java
@@ -6,7 +6,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,28 +15,62 @@ public class MainActivity extends AppCompatActivity {
     final String LOG_TAG = "myLogs";
 
     final String DB_NAME = "staff"; // name DB
-    final int DB_VERSION = 1; // version DB
+    final int DB_VERSION = 2; // version DB
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void onCreate(SQLiteDatabase db) {
+        Log.d(LOG_TAG, " --- onCreate database --- ");
 
-        DBHelper dbh = new DBHelper(this);
-        SQLiteDatabase db = dbh.getWritableDatabase();
-        Log.d(LOG_TAG, " --- Staff db v." + db.getVersion() + " --- ");
-        writeStaff(db);
-        dbh.close();
+        String[] people_name = { "Ivan", "Marya", "Peter", "Anton", "Dasha", "Boris", "Kostya", "Igor" };
+        int[] people_posid = { 2, 3, 2, 2, 3, 1, 2, 4 };
+
+        int[] position_id = { 1, 2, 3, 4 };
+        String[] position_name = { "director", "programmer",
+                "accountant", "security guard" };
+        int[] position_salary = { 15000, 13000, 10000, 8000 };
+
+        ContentValues cv = new ContentValues();
+
+        db.execSQL("create table position (" + "id integer primary key,"
+                + "name text, salary integer" + ");");
+
+        for (int i = 0; i < position_id.length; i++) {
+            cv.clear();
+            cv.put("id", position_id[i]);
+            cv.put("name", position_name[i]);
+            cv.put("salary", position_salary[i]);
+            db.insert("position", null, cv);
+        }
+
+        db.execSQL("create table people ("
+                + "id integer primary key autoincrement,"
+                + "name text, posid integer);");
+
+        for (int i = 0; i < people_name.length; i++) {
+            cv.clear();
+            cv.put("name", people_name[i]);
+            cv.put("posid", people_posid[i]);
+            db.insert("people", null, cv);
+        }
     }
 
-    // data request. output to the log
     private void writeStaff(SQLiteDatabase db) {
         Cursor c = db.rawQuery("select * from people", null);
         logCursor(c, "Table people");
         c.close();
+
+        c = db.rawQuery("select * from position", null);
+        logCursor(c, "Table position");
+        c.close();
+
+        String sqlQuery = "select PL.name as Name, PS.name as Position, salary as Salary "
+                + "from people as PL "
+                + "inner join position as PS "
+                + "on PL.posid = PS.id ";
+        c = db.rawQuery(sqlQuery, null);
+        logCursor(c, "inner join");
+        c.close();
     }
 
-    // output to the log data from the cursor
     void logCursor(Cursor c, String title) {
         if (c != null) {
             if (c.moveToFirst()) {
@@ -107,7 +140,59 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.d(LOG_TAG, " --- onUpgrade database from " + oldVersion
+                    + " to " + newVersion + " version --- ");
 
+            if (oldVersion == 1 && newVersion == 2) {
+
+                ContentValues cv = new ContentValues();
+
+                int[] position_id = { 1, 2, 3, 4 };
+                String[] position_name = { "director", "programmer",
+                        "accountant", "security guard" };
+                int[] position_salary = { 15000, 13000, 10000, 8000 };
+
+                db.beginTransaction();
+                try {
+                    db.execSQL("create table position ("
+                            + "id integer primary key,"
+                            + "name text, salary integer);");
+
+                    for (int i = 0; i < position_id.length; i++) {
+                        cv.clear();
+                        cv.put("id", position_id[i]);
+                        cv.put("name", position_name[i]);
+                        cv.put("salary", position_salary[i]);
+                        db.insert("position", null, cv);
+                    }
+
+                    db.execSQL("alter table people add column posid integer;");
+
+                    for (int i = 0; i < position_id.length; i++) {
+                        cv.clear();
+                        cv.put("posid", position_id[i]);
+                        db.update("people", cv, "position = ?",
+                                new String[] { position_name[i] });
+                    }
+
+                    db.execSQL("create temporary table people_tmp ("
+                            + "id integer, name text, position text, posid integer);");
+
+                    db.execSQL("insert into people_tmp select id, name, position, posid from people;");
+                    db.execSQL("drop table people;");
+
+                    db.execSQL("create table people ("
+                            + "id integer primary key autoincrement,"
+                            + "name text, posid integer);");
+
+                    db.execSQL("insert into people select id, name, posid from people_tmp;");
+                    db.execSQL("drop table people_tmp;");
+
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+            }
         }
     }
 }
