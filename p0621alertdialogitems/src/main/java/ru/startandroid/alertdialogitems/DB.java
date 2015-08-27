cf7be0683dd0d4d925e2a0bdbb5e238da4903cfb
Login Viktor <batazor111@gmail.com>
2015-08-27 21:16:17 +0300

    Lesson 62. Create module p0621alertdialogitems (AlertDialogItems)

---
+++ p0621alertdialogitems/src/main/java/ru/startandroid/alertdialogitems/DB.java
@@ -0,0 +1,75 @@
+package ru.startandroid.alertdialogitems;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DB {
+
+    private static final String DB_NAME = "mydb";
+    private static final int DB_VERSION = 1;
+    private static final String DB_TABLE = "mytab";
+
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_TXT = "txt";
+
+    private static final String DB_CREATE =
+            "create table " + DB_TABLE + "(" +
+                    COLUMN_ID + " integer primary key, " +
+                    COLUMN_TXT + " text" +
+                    ");";
+
+    private final Context mCtx;
+
+
+    private DBHelper mDBHelper;
+    private SQLiteDatabase mDB;
+
+    public DB(Context ctx) {
+        mCtx = ctx;
+    }
+
+    public void open() {
+        mDBHelper = new DBHelper(mCtx, DB_NAME, null, DB_VERSION);
+        mDB = mDBHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        if (mDBHelper!=null) mDBHelper.close();
+    }
+
+    public Cursor getAllData() {
+        return mDB.query(DB_TABLE, null, null, null, null, null, null);
+    }
+
+    public void changeRec(int id, String txt) {
+        ContentValues cv = new ContentValues();
+        cv.put(COLUMN_TXT, txt);
+        mDB.update(DB_TABLE, cv, COLUMN_ID + " = " + id, null);
+    }
+
+    private class DBHelper extends SQLiteOpenHelper {
+
+        public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+            super(context, name, factory, version);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(DB_CREATE);
+
+            ContentValues cv = new ContentValues();
+            for (int i = 1; i < 5; i++) {
+                cv.put(COLUMN_ID, i);
+                cv.put(COLUMN_TXT, "sometext " + i);
+                db.insert(DB_TABLE, null, cv);
+            }
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        }
+    }
+}
---
+++ p0621alertdialogitems/src/main/java/ru/startandroid/alertdialogitems/MainActivity.java
@@ -0,0 +1,138 @@
+package ru.startandroid.alertdialogitems;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    final String LOG_TAG = "myLogs";
+
+    final int DIALOG_ITEMS = 1;
+    final int DIALOG_ADAPTER = 2;
+    final int DIALOG_CURSOR = 3;
+    int cnt = 0;
+    DB db;
+    Cursor cursor;
+
+    String data[] = { "one", "two", "three", "four" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = new DB(this);
+        db.open();
+        cursor = db.getAllData();
+        startManagingCursor(cursor);
+    }
+
+    public void onclick(View v) {
+        changeCount();
+        switch (v.getId()) {
+            case R.id.btnItems:
+                showDialog(DIALOG_ITEMS);
+                break;
+            case R.id.btnAdapter:
+                showDialog(DIALOG_ADAPTER);
+                break;
+            case R.id.btnCursor:
+                showDialog(DIALOG_CURSOR);
+                break;
+            default:
+                break;
+        }
+    }
+
+    protected Dialog onCreateDialog(int id) {
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        switch (id) {
+            case DIALOG_ITEMS:
+                adb.setTitle(R.string.items);
+                adb.setItems(data, myClickListener);
+                break;
+            case DIALOG_ADAPTER:
+                adb.setTitle(R.string.adapter);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                        android.R.layout.select_dialog_item, data);
+                adb.setAdapter(adapter, myClickListener);
+                break;
+            case DIALOG_CURSOR:
+                adb.setTitle(R.string.cursor);
+                adb.setCursor(cursor, myClickListener, DB.COLUMN_TXT);
+                break;
+        }
+        return adb.create();
+    }
+
+    protected void onPrepareDialog(int id, Dialog dialog) {
+        AlertDialog aDialog = (AlertDialog) dialog;
+        ListAdapter lAdapter = aDialog.getListView().getAdapter();
+
+        switch (id) {
+            case DIALOG_ITEMS:
+            case DIALOG_ADAPTER:
+                if (lAdapter instanceof BaseAdapter) {
+                    BaseAdapter bAdapter = (BaseAdapter) lAdapter;
+                    bAdapter.notifyDataSetChanged();
+                }
+                break;
+            case DIALOG_CURSOR:
+                break;
+            default:
+                break;
+        }
+    };
+
+    DialogInterface.OnClickListener myClickListener = new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+            Log.d(LOG_TAG, "which = " + which);
+        }
+    };
+
+    void changeCount() {
+        cnt++;
+        data[3] = String.valueOf(cnt);
+        db.changeRec(4, String.valueOf(cnt));
+        cursor.requery();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        db.close();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
