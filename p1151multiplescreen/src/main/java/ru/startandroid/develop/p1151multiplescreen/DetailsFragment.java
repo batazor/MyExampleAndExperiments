b34c17448e3b4f32e2d2c4e60f623464429f72b2
Login Viktor <batazor111@gmail.com>
2015-08-30 17:05:15 +0300

    Lesson 115. Create module p1151multiplescreen (MultipleScreen)

---
+++ p1151multiplescreen/src/main/java/ru/startandroid/develop/p1151multiplescreen/DetailsFragment.java
@@ -0,0 +1,32 @@
+package ru.startandroid.develop.p1151multiplescreen;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DetailsFragment extends Fragment {
+
+    public static DetailsFragment newInstance(int pos) {
+        DetailsFragment details = new DetailsFragment();
+        Bundle args = new Bundle();
+        args.putInt("position", pos);
+        details.setArguments(args);
+        return details;
+    }
+
+    int getPosition() {
+        return getArguments().getInt("position", 0);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.details, container, false);
+        TextView tv = (TextView) v.findViewById(R.id.tvText);
+        tv.setText(getResources().getStringArray(R.array.content)[getPosition()]);
+        return v;
+    }
+}
