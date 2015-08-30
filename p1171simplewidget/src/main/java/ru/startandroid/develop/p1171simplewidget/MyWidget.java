deddf43af6494947dead0b435128f710b44cfe31
Login Viktor <batazor111@gmail.com>
2015-08-30 17:46:00 +0300

    Lesson 117. Create module p1171simplewidget (SimpleWidget)

---
+++ p1171simplewidget/src/main/java/ru/startandroid/develop/p1171simplewidget/MyWidget.java
@@ -0,0 +1,39 @@
+package ru.startandroid.develop.p1171simplewidget;
+
+import java.util.Arrays;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.util.Log;
+
+public class MyWidget extends AppWidgetProvider {
+
+    final String LOG_TAG = "myLogs";
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        Log.d(LOG_TAG, "onEnabled");
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        Log.d(LOG_TAG, "onUpdate " + Arrays.toString(appWidgetIds));
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+        Log.d(LOG_TAG, "onDeleted " + Arrays.toString(appWidgetIds));
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        Log.d(LOG_TAG, "onDisabled");
+    }
+
+}
