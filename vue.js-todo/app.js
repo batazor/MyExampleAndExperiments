a9ef462c13bbcb8bd0e640ec3512c3ba2a262951
Login Viktor <batazor111@gmail.com>
2015-09-12 18:34:54 +0300

    Vue.js-todo: The End.

--- vue.js-todo/app.js
+++ vue.js-todo/app.js
@@ -37,28 +37,51 @@ new Vue({
         {
           id: 3,
           name: 'SXSW',
-          description: 'Music, film and interactive festival in Austin. TX.',
+          description: 'Music, film and interactive festival in Austin, TX.',
           date: '2016-03-11'
         }
       ];
       // $set is a convenience method provided by Vue that is similar to pushing
       // data onto an array
       this.$set('events', events);
+
+      // If we had a back end with an events endpoint setup that responds to GET requests
+      // this.$http.get('api/events').success(function(events) {
+      //   this.$set('events', events);
+      // }).error(function(error) {
+      //   console.log(error);
+      // });
     },
 
     // Adds an event to the existing events array
     addEvent: function() {
-      if (this.event.name) {
+      if(this.event.name) {
         this.events.push(this.event);
         this.event = { name: '', description: '', date: '' };
       }
+
+      // If we had an endpoint set up that responds to POST requests
+      // this.$http.post('api/events', this.event).success(function(response) {
+      //   this.events.push(this.event);
+      //   console.log("Event added!");
+      // }).error(function(error) {
+      //   console.log(error);
+      // });
     },
 
+    // Delete the event after the user confirms
     deleteEvent: function(index) {
-      if (confirm("Are you sure you want to delete this event?")) {
+      if(confirm("Are you sure you want to delete this event?")) {
         // $remove is a Vue convenience method similar to splice
         this.events.$remove(index);
       }
+
+      // We could also delete an event if we had the events endpoint set up to delete data
+      // this.$http.delete('api/events', index).success(function(response) {
+      //   this.events.$remove(index);
+      // }).error(function(error) {
+      //   console.log(error);
+      // });
     }
   }
 })
