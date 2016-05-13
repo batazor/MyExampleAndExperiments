1c893e29c2cdbbd6dad09b43f1f6e0350541c856
batazor <batazor111@gmail.com>
2016-05-13 18:25:26 +0300

    Pro React: initial commit

---
+++ JavaScript/React/Pro React/README.md
@@ -0,0 +1,6 @@
+# Pro React
+
+## Link
+
++ http://www.apress.com/9781484212615
++ http://www.amazon.com/Pro-React-Cassio-Sousa-Antonio/dp/1484212614
---
+++ JavaScript/React/Pro React/webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  entry: [
+    './source/App.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel'
+      }
+    ]
+  }
+};
