e0a8f9c638bafb75e6abe7905ed20bcf7843a812
Login Viktor <batazor111@gmail.com>
2016-05-17 06:09:32 +0300

    Pro React: Shopping List animated

--- JavaScript/React/Pro React/First App/src/App.js
+++ JavaScript/React/Pro React/First App/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// components
 import Hello from './components/Hello';
 import List from './components/List';
 import CommentForm from './components/CommentForm';
 import Search from './components/Search';
 import Greeter from './components/Greeter';
 import ContactsApp from './components/ContactsApp';
+import ShoppingList from './components/ShoppingList'
 
+// data
 import contactsData from './components/ContactsApp/email';
 
-ReactDOM.render(<ContactsApp contacts={ contactsData } />, document.getElementById('root'));
+ReactDOM.render(<ShoppingList />, document.getElementById('root'));
