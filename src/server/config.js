a55dcd213acb5fe68721a926546cce397eeef55a
Login Viktor <batazor111@gmail.com>
2016-05-27 00:55:29 +0300

    Refactor config file

--- src/server/config.js
+++ src/server/config.js
@@ -7,7 +7,7 @@ export const APP_NAME = process.env.APP_NAME || 'LMap';
 export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 4000;
 export const DOMAIN = process.env.DAMAIN || `http://127.0.0.1:${ PORT }`;
 export const ENV = process.env.NODE_ENV || 'develop';
-export const DEBUG =process.env.NODE_ENV === 'develop';
+export const DEBUG = process.env.NODE_ENV === 'develop';
 
 // Set analytics services ======================================================
 export const analytics = {
@@ -24,7 +24,7 @@ export const dbConfig = {
 // Set Auth ====================================================================
 export const authConfig = {
   serverCode: {
-    secret: 'mySecretCode',
+    secret: process.env.AUTH_SECRET_CODE,
     resave: true,
     saveUnitialized: true
   },
