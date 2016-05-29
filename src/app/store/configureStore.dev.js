19cd186f85340043d7b6ef636a56e4c623ef1e2c
Login Viktor <batazor111@gmail.com>
2016-05-29 12:53:36 +0300

    Add logger for Front-End

--- src/app/store/configureStore.dev.js
+++ src/app/store/configureStore.dev.js
@@ -1,11 +1,25 @@
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
 import rootReducer from '../reducers'
 
 export default function configureStore(initialState) {
-  return createStore(
+  const logger = createLogger({
+    duration: true
+  })
+
+  const store = createStore(
     rootReducer,
     initialState,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, logger)
   )
+
+  if (module.hot) {
+    module.hot.accept('../reducers', () => {
+      const nextRootReducer = require('../reducers')
+      store.replaceReducer(nextRootReducer)
+    })
+  }
+
+  return store
 }
