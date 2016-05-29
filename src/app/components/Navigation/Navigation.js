f0bb3fa02cce5793f585c544c69b2aca54b34885
Login Viktor <batazor111@gmail.com>
2016-05-29 23:06:45 +0300

    Fix component Navigation

--- src/app/components/Navigation/Navigation.js
+++ src/app/components/Navigation/Navigation.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react' // eslint-disable-line no-unused-vars
-import { Link } from 'react-router'
 import s from './Navigation.scss'
-import Button from '../Button'
 import NavLink from '../NavLink'
 
 export default class Navigation extends Component {
   render() {
     return (
       <div className={s.container}>
-        <Link to='/'>
-          <Button style='btnOne'>Home</Button>
-        </Link>
+        <NavLink to='/' title='Home' onlyActiveOnIndex={true} />
         <NavLink to='/admin' title='Admin' />
         <NavLink to='/maps' title='Maps' />
         <NavLink to='/auth' title='Auth' />
