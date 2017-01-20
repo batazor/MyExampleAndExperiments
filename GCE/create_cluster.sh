14bc23156b9b7c0e4b0b8f66fa061df063ac533d
Login Viktor <batazor111@gmail.com>
2017-01-20 04:39:39 +0300

    GCE cluster: update version image VM

--- GCE/create_cluster.sh
+++ GCE/create_cluster.sh
@@ -3,5 +3,6 @@
 # gce-europe-west1
 gcloud container clusters create gce-europe-west1 \
   --zone=europe-west1-b \
+  --image-type=cos:1.5.1 \
   --num-nodes=5 \
   --scopes "cloud-platform,storage-ro,logging-write,monitoring-write,service-control,service-management,https://www.googleapis.com/auth/ndev.clouddns.readwrite"
