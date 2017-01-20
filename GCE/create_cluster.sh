472c224d6ebdf7a64e1d6d5600f46b1ff4b7cd18
Login Viktor <batazor111@gmail.com>
2017-01-20 05:01:50 +0300

    GCE cluster: update version image VM

--- GCE/create_cluster.sh
+++ GCE/create_cluster.sh
@@ -7,10 +7,10 @@ gcloud container clusters create gce-europe-west1 \
   --disk-size "100" \
   --scopes "cloud-platform,storage-ro,logging-write,monitoring-write,service-control,service-management,https://www.googleapis.com/auth/ndev.clouddns.readwrite" \
   --enable-kubernetes-alpha \
-  --num-nodes "5" \
+  --num-nodes "3" \
   --network "default" \
   --enable-cloud-logging \
   --no-enable-cloud-monitoring \
   --enable-autoscaling \
-  --min-nodes "5" \
-  --max-nodes "10"
+  --min-nodes "3" \
+  --max-nodes "4"
