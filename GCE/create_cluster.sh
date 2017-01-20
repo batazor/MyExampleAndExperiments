213f3ccac299a727e22a1a1f04c57f91c2b6795e
Login Viktor <batazor111@gmail.com>
2017-01-20 04:50:08 +0300

    GCE cluster: update version image VM

--- GCE/create_cluster.sh
+++ GCE/create_cluster.sh
@@ -2,7 +2,17 @@
 
 # gce-europe-west1
 gcloud container clusters create gce-europe-west1 \
-  --zone=europe-west1-b \
-  --image-type=cos:1.5.1 \
-  --num-nodes=5 \
-  --scopes "cloud-platform,storage-ro,logging-write,monitoring-write,service-control,service-management,https://www.googleapis.com/auth/ndev.clouddns.readwrite"
+  --zone "europe-west1-b" \
+  --machine-type "n1-standard-8" \
+  --image-type "GCI" \
+  --disk-size "100" \
+  --local-ssd-count "2" \
+  --scopes "cloud-platform,storage-ro,logging-write,monitoring-write,service-control,service-management,https://www.googleapis.com/auth/ndev.clouddns.readwrite" \
+  --enable-kubernetes-alpha \
+  --num-nodes "5" \
+  --network "default" \
+  --enable-cloud-logging \
+  --no-enable-cloud-monitoring \
+  --enable-autoscaling \
+  --min-nodes "5" \
+  --max-nodes "10"
