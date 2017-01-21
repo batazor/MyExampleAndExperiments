6f83c62adf44280360ea47230cfb5d7454076c14
Login Viktor <batazor111@gmail.com>
2017-01-21 17:14:36 +0300

    Run four clusters for GCE

--- GCE/create_cluster.sh
+++ GCE/create_cluster.sh
@@ -1,7 +1,52 @@
 # Use the gcloud container clusters create command to create a Kubernetes clusters in the following zones:
 
+gcloud container clusters create gce-asia-east1 \
+  --zone asia-east1-b \
+  --machine-type "n1-highmem-2" \
+  --image-type "GCI" \
+  --disk-size "100" \
+  --scopes "cloud-platform,storage-ro,logging-write,monitoring-write,service-control,service-management,https://www.googleapis.com/auth/ndev.clouddns.readwrite" \
+  --enable-kubernetes-alpha \
+  --num-nodes "3" \
+  --network "default" \
+  --enable-cloud-logging \
+  --no-enable-cloud-monitoring \
+  --enable-autoscaling \
+  --min-nodes "3" \
+  --max-nodes "4"
+
 gcloud container clusters create gce-europe-west1 \
-  --zone "europe-west1-b" \
+  --zone europe-west1-b \
+  --machine-type "n1-highmem-2" \
+  --image-type "GCI" \
+  --disk-size "100" \
+  --scopes "cloud-platform,storage-ro,logging-write,monitoring-write,service-control,service-management,https://www.googleapis.com/auth/ndev.clouddns.readwrite" \
+  --enable-kubernetes-alpha \
+  --num-nodes "3" \
+  --network "default" \
+  --enable-cloud-logging \
+  --no-enable-cloud-monitoring \
+  --enable-autoscaling \
+  --min-nodes "3" \
+  --max-nodes "4"
+
+gcloud container clusters create gce-us-east1 \
+  --zone=us-east1-b \
+  --machine-type "n1-highmem-2" \
+  --image-type "GCI" \
+  --disk-size "100" \
+  --scopes "cloud-platform,storage-ro,logging-write,monitoring-write,service-control,service-management,https://www.googleapis.com/auth/ndev.clouddns.readwrite" \
+  --enable-kubernetes-alpha \
+  --num-nodes "3" \
+  --network "default" \
+  --enable-cloud-logging \
+  --no-enable-cloud-monitoring \
+  --enable-autoscaling \
+  --min-nodes "3" \
+  --max-nodes "4"
+
+gcloud container clusters create gce-us-central1 \
+  --zone=us-central1-b \
   --machine-type "n1-highmem-2" \
   --image-type "GCI" \
   --disk-size "100" \
