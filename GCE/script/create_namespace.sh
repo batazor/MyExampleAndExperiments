3e5b42cae8e2d9300358d6d30d5222828a960d0a
Login Viktor <batazor111@gmail.com>
2017-01-21 22:12:05 +0300

    Refactoring

---
+++ GCE/script/create_namespace.sh
@@ -0,0 +1,4 @@
+export GCP_PROJECT=$(gcloud config list --format='value(core.project)')
+
+kubectl --context="gke_${GCP_PROJECT}_us-central1-b_gce-us-central1" \
+  create -f ../../k8s/addons/federation/federation-ns.yml
