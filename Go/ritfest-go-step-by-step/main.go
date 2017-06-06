9f1424469926a411896ad8bfef9bf303611245c4
Login Victor <batazor111@gmail.com>
2017-06-06 14:30:04 +0300

    Step 1

--- Go/ritfest-go-step-by-step/main.go
+++ Go/ritfest-go-step-by-step/main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"github.com/takama/router"
+	"os"
+	"github.com/joho/godotenv"
+	"log"
 )
 
 // Run server: go build && step-by-step
 // Try requests: curl http://127.0.0.1:8000/test
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	port := os.Getenv("HTTP_PORT")
+	if len(port) == 0 {
+		log.Fatal("Required parameter service port is not set")
+	}
+
 	r := router.New()
 	r.GET("/", home)
-	r.Listen(":8000")
+
+	r.Listen(":" + port)
 }
