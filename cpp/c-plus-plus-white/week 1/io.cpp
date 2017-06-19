24effcf7904ed5742a120c055d8f11d8ea68d879
Login Victor <batazor111@gmail.com>
2017-06-20 00:05:21 +0300

    cpp io

---
+++ cpp/c-plus-plus-white/week 1/io.cpp
@@ -0,0 +1,24 @@
+//
+// Created by batazor on 19.06.17.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int x = 4;
+    string y = "hello";
+
+    cout << x << " " << y << endl;
+
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+
+    string name, surname;
+    cin >> name >> surname;
+    cout << "Hello, " << name << " " << surname << endl;
+
+    return 0;
+}
