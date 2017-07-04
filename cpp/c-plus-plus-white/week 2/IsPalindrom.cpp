c9b2f6f3cfed40bb42e99ef2b8e7615154367d34
Login Victor <batazor111@gmail.com>
2017-07-04 21:21:02 +0300

    Add IsPalindrom.cpp

---
+++ cpp/c-plus-plus-white/week 2/IsPalindrom.cpp
@@ -0,0 +1,33 @@
+//
+// Created by batazor on 04.07.17.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int IsPalindrom(string word) {
+    if (word.size() < 2) return true;
+
+    int sizeCount = word.size() / 2 + 1;
+    bool IsPalindrom = true;
+
+    for (int i = 0; i < sizeCount; i++) {
+        if (word.at(i) != word.at(word.size() - i - 1)) {
+            IsPalindrom = false;
+            break;
+        }
+    }
+
+    return IsPalindrom;
+}
+
+int main() {
+    cout << IsPalindrom("madam") << endl;
+    cout << IsPalindrom("hello") << endl;
+    cout << IsPalindrom("madam") << endl;
+    cout << IsPalindrom("bro") << endl;
+    cout << IsPalindrom("34jtj43") << endl;
+    cout << IsPalindrom("34gfd456") << endl;
+    return 0;
+}
