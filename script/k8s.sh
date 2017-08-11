8e4324d1886aef98a617527e2a1c5acd1f769139
Victor Login <batazor111@gmail.com>
2017-08-11 20:21:53 +0300

    add func: install kubectl

--- script/k8s.sh
+++ script/k8s.sh
@@ -18,16 +18,19 @@ install_kubectl() {
   ADMIN_KEY="./cert/admin-key.pem"
   ADMIN_CERT="./cert/admin.pem"
 
-  # Download
+  print_green " - Download kubectl"
   curl -o ~/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/${K8S_VER}/bin/linux/amd64/kubectl
   chmod +x ~/bin/kubectl
 
-  # Setting
+  print_green " - Edit .bashrc"
+  echo "export PATH=$PATH:~/bin" > ~/.bashrc
+
+  print_green " - Setting kubectl"
   kubectl config set-cluster default-cluster --server=https://${MASTER_HOST}:${APISERVER_PORT} --certificate-authority=${CA_CERT}
   kubectl config set-credentials default-admin --certificate-authority=${CA_CERT} --client-key=${ADMIN_KEY} --client-certificate=${ADMIN_CERT}
   kubectl config set-context default-system --cluster=default-cluster --user=default-admin
   kubectl config use-context default-system
 
-  # Test
+  print_green " - Test kubectl. Get node"
   kubectl get node
 }
