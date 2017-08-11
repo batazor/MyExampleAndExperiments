f6ae8a069ee49de795591cc59745ae6a29914cd6
Victor Login <batazor111@gmail.com>
2017-08-11 20:19:37 +0300

    add func: install kubectl

--- script/k8s.sh
+++ script/k8s.sh
@@ -12,3 +12,22 @@ clean_k8s_conf() {
   sudo systemctl stop kubelet docker flanneld etcd2
   sudo systemctl start etcd2 flanneld
 }
+
+install_kubectl() {
+  CA_CERT="./cert/ca.pem"
+  ADMIN_KEY="./cert/admin-key.pem"
+  ADMIN_CERT="./cert/admin.pem"
+
+  # Download
+  curl -o ~/bin/kubectl https://storage.googleapis.com/kubernetes-release/release/${K8S_VER}/bin/linux/amd64/kubectl
+  chmod +x ~/bin/kubectl
+
+  # Setting
+  kubectl config set-cluster default-cluster --server=https://${MASTER_HOST}:${APISERVER_PORT} --certificate-authority=${CA_CERT}
+  kubectl config set-credentials default-admin --certificate-authority=${CA_CERT} --client-key=${ADMIN_KEY} --client-certificate=${ADMIN_CERT}
+  kubectl config set-context default-system --cluster=default-cluster --user=default-admin
+  kubectl config use-context default-system
+
+  # Test
+  kubectl get node
+}
