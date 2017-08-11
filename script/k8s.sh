7bb503daf379e797061f65fd1ab0b1db6ffc12b3
Victor Login <batazor111@gmail.com>
2017-08-11 19:55:55 +0300

    Update

--- script/k8s.sh
+++ script/k8s.sh
@@ -3,6 +3,8 @@ clean_k8s_conf() {
   sudo /bin/rm -rf /etc/flannel
   sudo /bin/rm -rf /etc/kubernetes/*
   sudo /bin/rm -rf /etc/systemd/system/*
+  sudo /bin/rm -rf /opt/cni/bin
+  sudo /bin/rm -rf /srv/kubernetes/*
 
   print_green "Disable services"
   sudo systemctl daemon-reload
