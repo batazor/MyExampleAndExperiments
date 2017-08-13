8921c3de78ccf0e6a473052ffa7c190da9b7b7cf
Login Victor <batazor111@gmail.com>
2017-08-13 15:23:36 +0300

    Fix config for worker

--- script/worker.sh
+++ script/worker.sh
@@ -28,8 +28,7 @@ generate_config_worker() {
   for yaml in `find template/worker -type f | grep "\.template"`; do
     path=$(echo $yaml | sed -r 's/^template/dist/g')
 
-    path=$(echo $path | sed -r 's/(\.template|master|worker[!/])//g')
-    path=$(echo $path | sed -r 's/dist\//dist/g')
+    path=$(echo $path | sed -r 's/(\.template|master[!/]|worker[!/])//g')
     directory=$(echo $path | sed 's/\(.*\)\/.*/\1/')
     mkdir -p $directory
     eval "envsubst < ${yaml} > $path"
