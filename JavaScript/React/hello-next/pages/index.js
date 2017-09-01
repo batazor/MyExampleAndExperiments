2a65e38f8a61e9e1fc276656106a2eb302a4d143
Login Victor <batazor111@gmail.com>
2017-09-01 22:16:01 +0300

    Navigate Between Pages

--- JavaScript/React/hello-next/pages/index.js
+++ JavaScript/React/hello-next/pages/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Index = () => (
   <div>
+    <Link href="/about">
+      <button>Go to About Page</button>
+    </Link>
     <p>Hello Next.js</p>
   </div>
 );
