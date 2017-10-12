be817222cb31080d9451299aac6712c7df5ede51
Login Victor <batazor111@gmail.com>
2017-10-12 18:12:42 +0300

    Geenrete kubeconfig for worker

--- script/worker.sh
+++ script/worker.sh
@@ -11,6 +11,29 @@ ssl_worker() {
   print_green "TLS Assets"
 }
 
+generate_kubeconfig() {
+  print_green " - Generate kubeconfig"
+
+  kubectl config set-cluster default-cluster \
+    --certificate-authority=${CA_CERT} \
+    --embed-certs \
+    --server=https://${MASTER_HOST}:${APISERVER_PORT} \
+    --kubeconfig=${HOSTNAME}.kubeconfig
+
+  kubectl config set-credentials system:node \
+    --client-certificate=${ADMIN_CERT} \
+    --client-key=${ADMIN_KEY} \
+    --embed-certs=true \
+    --kubeconfig=${HOSTNAME}.kubeconfig
+
+  kubectl config set-context default-cluster \
+    --cluster=default-cluster \
+    --user=system:node \
+    --kubeconfig=${HOSTNAME}.kubeconfig
+
+  kubectl config use-context default --kubeconfig=${HOSTNAME}.kubeconfig
+}
+
 generate_config_worker() {
   print_green "Clear dist" && rm -rf dist
 
@@ -24,6 +47,8 @@ generate_config_worker() {
   done
 
   sudo cp -R dist/* /
+
+  generate_kubeconfig
 }
 
 start_worker() {
@@ -34,8 +59,8 @@ start_worker() {
 }
 
 add_worker() {
-  yes_or_no "Generate new cert?" && new_ssl_worker;
+  yes_or_no "Generate new cert?" && new_ssl_worker
   print_green "Move ssh cert" && ssl_worker
-  yes_or_no "Generate new config?" && generate_config_worker;
-  yes_or_no "Start worker?" && start_worker;
+  yes_or_no "Generate new config?" && generate_config_worker
+  yes_or_no "Start worker?" && start_worker
 }
