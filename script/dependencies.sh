4f87516c91b09ead588f5360585b6c22e5de8e07
Login Victor <batazor111@gmail.com>
2017-10-13 11:41:54 +0300

    Add preload image

--- script/dependencies.sh
+++ script/dependencies.sh
@@ -5,23 +5,29 @@ install_cfssl() {
   curl -s -L -o ~/bin/cfssl https://pkg.cfssl.org/R1.2/cfssl_linux-amd64
   curl -s -L -o ~/bin/cfssljson https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
   chmod +x ~/bin/{cfssl,cfssljson}
-  print_green "Success!"
+  print_green "- success!"
 }
 
 install_kubectl() {
   print_green "Install kubectl."
   mkdir -p ~/bin
-  print_green "Download. Please wait..."
+  print_green "- Download. Please wait..."
   curl -O https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_TAG}/bin/linux/amd64/kubectl
   mv kubectl ~/bin/kubectl
   chmod +x ~/bin/kubectl
-  print_green "Success!"
+  print_green "- success!"
+}
+
+preload_image() {
+  print_green "Download $KUBELET_IMAGE_URL:$KUBELET_IMAGE_TAG."
+  docker pull ${KUBELET_IMAGE_URL}:${KUBELET_IMAGE_TAG}
+  print_green "- success!"
 }
 
 install_dependencies() {
   echo;echo;echo;
   PS3="Please enter your choice: "
-  options=("cfssl" "kubectl" "Quit")
+  options=("cfssl" "kubectl" "preinstall image" "Quit")
   select opt in "${options[@]}"
   do
       case $opt in
@@ -30,6 +36,9 @@ install_dependencies() {
               ;;
           "kubectl")
               install_kubectl
+              ;;
+          "preload image")
+              preload_image
               ;;
           "Quit")
               break
