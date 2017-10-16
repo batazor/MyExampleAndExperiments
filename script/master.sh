f5648a4a7585143fe4958efddb0a1b780f0f4000
Login Victor <batazor111@gmail.com>
2017-10-16 12:00:53 +0300

    reload systemd services

--- script/master.sh
+++ script/master.sh
@@ -35,7 +35,7 @@ start_master() {
 
   sudo systemctl daemon-reload
   sudo systemctl enable --now flanneld docker kubelet
-  sudo systemctl reload flanneld docker kubelet
+  sudo systemctl restart flanneld docker kubelet
 }
 
 add_master() {
