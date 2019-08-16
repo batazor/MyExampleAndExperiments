ed50169d0d52b73a9d0b040a5e3214f8d3350322
Victor Login <batazor111@gmail.com>
2019-08-16 22:50:22 +0300

    work

--- first-network/byfn.sh
+++ first-network/byfn.sh
@@ -1,54 +1,10 @@
 #!/bin/bash
-#
-# Copyright IBM Corp All Rights Reserved
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# This script will orchestrate a sample end-to-end execution of the Hyperledger
-# Fabric network.
-#
-# The end-to-end verification provisions a sample Fabric network consisting of
-# two organizations, each maintaining two peers, and a “solo” ordering service.
-#
-# This verification makes use of two fundamental tools, which are necessary to
-# create a functioning transactional network with digital signature validation
-# and access control:
-#
-# * cryptogen - generates the x509 certificates used to identify and
-#   authenticate the various components in the network.
-# * configtxgen - generates the requisite configuration artifacts for orderer
-#   bootstrap and channel creation.
-#
-# Each tool consumes a configuration yaml file, within which we specify the topology
-# of our network (cryptogen) and the location of our certificates for various
-# configuration operations (configtxgen).  Once the tools have been successfully run,
-# we are able to launch our network.  More detail on the tools and the structure of
-# the network will be provided later in this document.  For now, let's get going...
 
 # prepending $PWD/../bin to PATH to ensure we are picking up the correct binaries
 # this may be commented out to resolve installed version of tools if desired
 export PATH=${PWD}/../bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=${PWD}
 export VERBOSE=false
-
-# Ask user for confirmation to proceed
-function askProceed() {
-  read -p "Continue? [Y/n] " ans
-  case "$ans" in
-  y | Y | "")
-    echo "proceeding ..."
-    ;;
-  n | N)
-    echo "exiting..."
-    exit 1
-    ;;
-  *)
-    echo "invalid response"
-    askProceed
-    ;;
-  esac
-}
 
 # Obtain CONTAINER_IDS and remove them
 # TODO Might want to make this optional - could clear other containers
@@ -252,23 +208,6 @@ function replacePrivateKey() {
   fi
 }
 
-# We will use the cryptogen tool to generate the cryptographic material (x509 certs)
-# for our various network entities.  The certificates are based on a standard PKI
-# implementation where validation is achieved by reaching a common trust anchor.
-#
-# Cryptogen consumes a file - ``crypto-config.yaml`` - that contains the network
-# topology and allows us to generate a library of certificates for both the
-# Organizations and the components that belong to those Organizations.  Each
-# Organization is provisioned a unique root certificate (``ca-cert``), that binds
-# specific components (peers and orderers) to that Org.  Transactions and communications
-# within Fabric are signed by an entity's private key (``keystore``), and then verified
-# by means of a public key (``signcerts``).  You will notice a "count" variable within
-# this file.  We use this to specify the number of peers per Organization; in our
-# case it's two peers per Org.  The rest of this template is extremely
-# self-explanatory.
-#
-# After we run the tool, the certs will be parked in a folder titled ``crypto-config``.
-
 # Generates Org certs using cryptogen tool
 function generateCerts() {
   which cryptogen
@@ -400,12 +339,6 @@ if [ "$MODE" == "up" ]; then
   EXPMODE="Starting"
 elif [ "$MODE" == "down" ]; then
   EXPMODE="Stopping"
-elif [ "$MODE" == "restart" ]; then
-  EXPMODE="Restarting"
-elif [ "$MODE" == "generate" ]; then
-  EXPMODE="Generating certs and genesis block"
-elif [ "$MODE" == "upgrade" ]; then
-  EXPMODE="Upgrading the network"
 else
   exit 1
 fi
@@ -448,26 +381,11 @@ while getopts "h?c:t:d:f:s:l:i:o:anv" opt; do
   esac
 done
 
-
-# Announce what was requested
-
-# ask for confirmation to proceed
-askProceed
-
 #Create the network using docker compose
 if [ "${MODE}" == "up" ]; then
   networkUp
 elif [ "${MODE}" == "down" ]; then ## Clear the network
   networkDown
-elif [ "${MODE}" == "generate" ]; then ## Generate Artifacts
-  generateCerts
-  replacePrivateKey
-  generateChannelArtifacts
-elif [ "${MODE}" == "restart" ]; then ## Restart the network
-  networkDown
-  networkUp
-elif [ "${MODE}" == "upgrade" ]; then ## Upgrade the network from version 1.2.x to 1.3.x
-  upgradeNetwork
 else
   exit 1
 fi
